dd1ff074d8441893d580a95f858c64f4b3abe16e
Michael Hackman <reedhackman@gmail.com>
2019-04-23 17:45:12 -0700

    bug fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use('/api/', require('./server/routes.js'))
 
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname + '../client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
 const port = process.env.PORT || 5000;
