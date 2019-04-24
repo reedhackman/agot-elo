a0bc93be2c8729b3c2310ac783d22bb572999596
Michael Hackman <reedhackman@gmail.com>
2019-04-23 20:16:03 -0700

    heroku table populate test

--- server/maintain.js
+++ server/maintain.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 const { Client, Pool } = require('pg')
+const getJson = require('get-json')
+const async = require('async')
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -8,6 +10,12 @@ const pool = new Pool({
 
 let page = 1
 let length = 0
+const url = 'https://thejoustingpavilion.com/api/v3/games?page='
+let newGames = []
+let createPlayersArray = []
+let playersToUpdate = []
+let updatePlayersArray = []
+let players = {}
 
 pool.query('SELECT * FROM position', (err, data) => {
   if(err){
@@ -37,4 +45,123 @@ const createPlayersTable = () => {
     if(err) throw err
     console.log('players table created in database')
   })
+}
+
+const createPlayer = (id, name) => {
+  players[id] = {
+    name: name,
+    id: id,
+    wins: 0,
+    losses: 0,
+    rating: 1200,
+    percent: 0,
+    played: 0
+  }
+  createPlayersArray.push((callback) => {
+    pool.query('INSERT INTO testplayers (id, name, rating, wins, losses, percent, played) VALUES ($1, $2, $3, $4, $5, $6, $7)', [id, name, 1200, 0, 0, 0, 0], (err, data) => {
+      if(err) throw err
+      console.log('created player id: ' + id)
+    })
+    callback()
+  })
+}
+
+const updateAllPlayers = () => {
+  playersToUpdate.forEach((id) => {
+    updatePlayersArray.push((callback) => {
+      let wins = players[id].wins
+      let losses = players[id].losses
+      pool.query('UPDATE testplayers SET wins = $1, losses = $2, rating = $3, percent = $5, played = $6 WHERE id = $4', [wins, losses, players[id].rating, id, wins / (wins + losses), wins + losses], (err, data) => {
+        if(err) throw err
+        console.log('updated player id: ' + id)
+      })
+      callback()
+    })
+  })
+  playersToUpdate = new Array
+}
+
+const check = () => {
+  getJson(url + page, (err, games) => {
+    if(err) throw err
+    console.log('page ' + page + ' length ' + games.length)
+    for(var i = length; i < games.length; i++){
+      newGames.push(games[i])
+    }
+    if(games.length == 50){
+      length = 0
+      page++
+      check()
+    }
+    else{
+      newGames.forEach((game) => {
+        process(game)
+      })
+      newGames = []
+      console.log('done with thejoustingpavilion')
+    }
+  })
+}
+
+const process = (game) => {
+  else if(game.p1_id < 1 || game.p2_id < 1){
+    return
+  }
+  else if(game.game_status != 100){
+    return
+  }
+  let winner = new Object
+  let loser = new Object
+  if(game.p1_points > game.p2_points){
+    winner = {
+      name: game.p1_name,
+      id: game.p1_id,
+      faction: game.p1_faction,
+      agenda: game.p1_agenda
+    }
+    loser = {
+      name: game.p2_name,
+      id: game.p2_id,
+      faction: game.p2_faction,
+      agenda: game.p2_agenda
+    }
+  }
+  else if(game.p1_points < game.p2_points){
+    winner = {
+      name: game.p2_name,
+      id: game.p2_id,
+      faction: game.p2_faction,
+      agenda: game.p2_agenda
+    }
+    loser = {
+      name: game.p1_name,
+      id: game.p1_id,
+      faction: game.p1_faction,
+      agenda: game.p1_agenda
+    }
+  }
+  else{
+    return
+  }
+  if(!(players[winner.id])){
+    createPlayer(winner.id, winner.name)
+  }
+  if(!(players[loser.id])){
+    createPlayer(loser.id, loser.name)
+  }
+  let qW = Math.pow(10, (players[winner.id].rating / 400))
+  let qL = Math.pow(10, (players[loser.id].rating / 400))
+  let eW = qW / (qW + qL)
+  let eL = qL / (qW + qL)
+  let k = 40
+  players[winner.id].rating += k * (1 - eW)
+  players[loser.id].rating += k * (0 - eL)
+  players[winner.id].wins++
+  players[loser.id].losses++
+  if(!(playersToUpdate.includes(winner.id))){
+    playersToUpdate.push(winner.id)
+  }
+  if(!(playersToUpdate.includes(loser.id))){
+    playersToUpdate.push(loser.id)
+  }
 }
